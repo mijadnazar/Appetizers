a5679aafd85f92f99015eec30659ea465dd33173
ijadnazar.mahdi <ijadnazar.mahdi@gmail.com>
2023-12-29 19:47:12 -0500

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Mehdi Ijadnazar on 10/8/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
