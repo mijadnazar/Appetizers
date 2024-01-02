7adf8ef0263cbaf68745a7f030c4df468f0958ff
ijadnazar.mahdi <ijadnazar.mahdi@gmail.com>
2024-01-01 21:25:44 -0500

    Add NetworkManager
    - Created NetworkManager to retrieve list of Appetizers

---
+++ Appetizers/AppetizerListViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  AppetizerListViewModel.swift
+//  Appetizers
+//
+//  Created by Mehdi Ijadnazar on 10/11/1402 AP.
+//
+
+import SwiftUI
+
+final class AppetizerListViewModel: ObservableObject {
+    @Published var appetizers: [Appetizer] = []
+    
+    func getAppetizers() {
+        NetworkManager.shared.getAppetizer { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+}
