cc2d6dbd9f3412e5419bbdd572cfc4d7eff889e8
ijadnazar.mahdi <ijadnazar.mahdi@gmail.com>
2024-01-02 17:26:39 -0500

    Add error handling to AppetizerListView
    - Added alerts to show appropriate error for networking code

--- Appetizers/AppetizerListViewModel.swift
+++ Appetizers/AppetizerListViewModel.swift
@@ -9,15 +9,25 @@ import SwiftUI
 
 final class AppetizerListViewModel: ObservableObject {
     @Published var appetizers: [Appetizer] = []
+    @Published var alertItem: AlertItem?
     
     func getAppetizers() {
         NetworkManager.shared.getAppetizer { result in
-            DispatchQueue.main.async {
+            DispatchQueue.main.async { [self] in
                 switch result {
                 case .success(let appetizers):
                     self.appetizers = appetizers
                 case .failure(let error):
-                    print(error.localizedDescription)
+                    switch error {
+                    case .invalidURL:
+                        alertItem = AlertContext.invalidURL
+                    case .invalidResponse:
+                        alertItem = AlertContext.invalidResponse
+                    case .invalidData:
+                        alertItem = AlertContext.invalidData
+                    case .unableToComplete:
+                        alertItem = AlertContext.unableToComplete
+                    }
                 }
             }
         }
