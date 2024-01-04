406cb2838168c1e08df672667d1e1977bcbc3e1e
ijadnazar.mahdi <ijadnazar.mahdi@gmail.com>
2024-01-04 16:40:46 -0500

    Create UI for AccountView
    Created basic UI and variables for AccountView

--- Appetizers/AccountView.swift
+++ Appetizers/AccountView.swift
@@ -8,10 +8,48 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthDate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+    
     var body: some View {
         NavigationView {
-            Text("Account")
-                .navigationTitle("Account")
+            Form {
+                Section {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.none)
+                        .autocorrectionDisabled(true)
+                    DatePicker(
+                        "Birthdate",
+                        selection: $birthDate,
+                        displayedComponents: .date
+                    )
+                    
+                    Button(action: {
+                        print("saved")
+                    }, label: {
+                        Text("Save Changes")
+                    })
+                } header: {
+                    Text("Account")
+                }
+                
+                Section {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                } header: {
+                    Text("Account")
+                }
+                .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
+
+            }
+            .navigationTitle("Account")
         }
     }
 }
